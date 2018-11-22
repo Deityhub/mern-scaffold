46586827718c9a379236f80f443436a026f145e8
Ojini Chizoba Jude <judoc96@gmail.com>
2018-11-22 14:08:42 +0100

    add message when server first loads

--- api/routes/hello.js
+++ api/routes/hello.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/todos", (req, res, next) => {
-  res.json({ message: "get all todos" });
+router.get("/", (req, res, next) => {
+  res.send("Hello world, welcome to your mern scaffold");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-const todoRoutes = require("./api/routes/todos");
+const helloRoute = require("./api/routes/hello");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.use("/api", todoRoutes);
+app.use("/api", helloRoute);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
